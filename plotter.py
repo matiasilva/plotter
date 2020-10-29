870567e07eef7e7b4754db04390d9cc3ccceb45f
Matias Silva <mw781@srcf.net>
2020-10-29 15:05:48 +0000

    Added better way to track inputs

--- plotter.py
+++ plotter.py
@@ -1,17 +1,40 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from uuid import uuid4
 
-# Data for plotting
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2 * np.pi * t)
+inputs = []
+inputs.append({
+	"name": "al",
+	"label": "Al alloy",
+	"lcolor": "b",
+	"textloc": (1.8, 1.5),
+	"textoff": (90, -25),
+})
+inputs.append({
+	"name": "pmma",
+	"label": "PMMA",
+	"lcolor": "g",
+	"textloc": (2.2, 2.6),
+	"textoff": (-15, 25),
+})
 
 fig, ax = plt.subplots()
-ax.plot(t, s)
 
-ax.set(xlabel='time (s)', ylabel='voltage (mV)',
-       title='About as simple as it gets, folks')
-ax.grid()
+for input in inputs:
+	name, label, lcolor, textloc, textoff = input.values()
+	load, disp, force = np.loadtxt(f"input/{name}.txt", delimiter='	', unpack=True)
+	ax.plot(load, disp, f"-D{lcolor}", label=label, markerfacecolor='r', markeredgecolor='k')
+	p, cov = np.polyfit(disp, load, 1, cov=True)
+	print(f"{label}: {np.sqrt(np.diag(cov))}")
+	ax.annotate(fr"k = {p[0]:.3f} kg $\mathregular{{mm^{-1}}}$",
+            xy=textloc, xycoords='data',
+            horizontalalignment='right', arrowprops={"arrowstyle":  "-|>"},
+			verticalalignment='center', xytext=textoff, textcoords='offset points', bbox=dict(facecolor='none', edgecolor='black', pad=5.0))
+	ax.errorbar(load, disp, yerr=50*0.002, xerr=8*0.01, ecolor='black', capsize=3, fmt="none")
 
-fig.savefig("test.png")
+ax.legend()
+
+ax.set(xlabel='deflection (mm)', ylabel='mass on load hanger (kg)')
+fig.savefig(f"output/test-{str(uuid4())[:4]}.png")
 plt.show()
